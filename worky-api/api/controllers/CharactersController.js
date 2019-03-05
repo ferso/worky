f7f556684033a8dfcfb9fee60202ba09fede61be
ferso <ferso@fersos-iMac.local>
2019-03-05 10:08:27 -0600

    directories,api,vueapp,scripts

---
+++ worky-api/api/controllers/CharactersController.js
@@ -0,0 +1,13 @@
+/**
+ * CharactersController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  index:  async (req, res)  => {
+    res.json({code:0,msg:'ok'});
+  }
+
+};
